6c01c0bf8787cecee9ddd7390e2d028fc6f44314
karen-rak <44555683+karen-rak@users.noreply.github.com>
2018-11-25 14:05:39 -0500

    Add files via upload

--- login.php
+++ login.php
@@ -1,22 +1,27 @@
 <?php
-include('config.php');
-if(isset($_POST['action']))
-{          
-    if($_POST['action']=="login")
-    {
-        $username = mysqli_real_escape_string($connection,$_POST['username']);
-        $password = mysqli_real_escape_string($connection,$_POST['password']);
-        $strSQL = mysqli_query($connection,"select name from users where username='".$username."' and password='".$password."'");
-        $Results = mysqli_fetch_array($strSQL);
-        if(count($Results)>=1)
-        {
-            $message = $Results['name']." Login Sucessfully!!";
-        }
-        else
-        {
-            $message = "Invalid email or password!!";
-        }        
-    }
-}
+$servername = "localhost";
+$username = "root";
+$passward = "";
+$dbname = "myDB";
+$tablename = "WebsiteUsers";
+//Create connection
+$connection = new mysqli($servername, $username, $passward, $dbname);
+
+$username   = mysqli_real_escape_string ($connection,$_POST['username']);
+$password   = mysqli_real_escape_string ($connection,$_POST['password']);
+
+$query = "SELECT * FROM WebsiteUsers WHERE username='$username' and pass='$password'";
  
+$result = mysqli_query($connection, $query) or die(mysqli_error($connection));
+$count = mysqli_num_rows($result);
+
+if ($count == 1){
+    echo "condragulatios";
+
+    }else{
+
+        $fmsg = "Invalid Login Credentials.";
+        echo $fmsg;
+}
+
 ?>
--- sign up.php
+++ sign up.php
@@ -1,21 +1,38 @@
 <?php
-include('config.php');
-if(isset($_POST['action']))
-{          
-    if($_POST['action']=="signup") {
-        $username       = mysqli_real_escape_string($connection,$_POST['username']);
-        $email      = mysqli_real_escape_string($connection,$_POST['email']);
-        $password   = mysqli_real_escape_string($connection,$_POST['password']);
-        $query = "SELECT email FROM users where email='".$email."'";
+$servername = "localhost";
+$username = "root";
+$passward = "";
+$dbname = "myDB";
+$tablename = "WebsiteUsers";
+//Create connection
+$connection = new mysqli($servername, $username, $passward, $dbname);
+
+
+        $fname       = mysqli_real_escape_string ($connection,$_POST['fname']);
+        $lname       = mysqli_real_escape_string ($connection,$_POST['lname']);
+        $email       = mysqli_real_escape_string ($connection,$_POST['email']);
+        $username   = mysqli_real_escape_string ($connection,$_POST['username']);
+        $password   = mysqli_real_escape_string ($connection,$_POST['password']);
+        $points     = "O";
+       
+       
+        $query = "SELECT email FROM WebsiteUsers where email='".$email."'";
         $result = mysqli_query($connection,$query);
         $numResults = mysqli_num_rows($result);
-    } 
-    elseif($numResults>=1){
-        $message = $email." Email already exist!!";
-    } else {
-            mysql_query("insert into users(username,email,password) values('".$username."','".$email."','".$password."')");
-            $message = "Signup Sucessfully!!";
+
+        if($numResults>=1){
+            "Email already exist!!";
+            echo $message;
+        } else {
+            $sql = "INSERT INTO WebsiteUsers (fname, lname, email, userName, pass, points) VALUES ('$fname', '$lname', '$email','$username', '$password', '$points')";
+
+            if ($connection->query($sql) === TRUE) {
+                $message = "Sign Up Sucessfully!!";
+                echo $message;
+            } else {
+                echo "fail" . $connection->error; 
+            }
         }
-}
  
+$connection->close();
 ?>
