10b433a675efb0822ca3af67fdc9f1c226d5e32b
karen-rak <44555683+karen-rak@users.noreply.github.com>
2018-12-07 00:17:26 -0500

    Add files via upload

--- AdminPage/admin.js
+++ AdminPage/admin.js
@@ -1,33 +1,40 @@
 ﻿var rowNum = 1;
 $(document).ready(function(){
-        $("#create-user").click(function(){
-            var first = $("#first").val();
-            var last = $("#last").val();
-            var amount = $("#amount").val();
-            var markup = //"<tr><th scope="+"row>" + rowNum + "</th>" +
-                            "<tr><td>" + rowNum + "</td>" +
-                            "<td>" + first + "</td>" +
-                            "<td>" + last + "</td>" +
-                            "<td>$" + amount + "</td>" +
-                            "<td><input type='checkbox'></td></tr>";
-            $("table tbody").append(markup);
-            rowNum++;
-            //clear value after submit
-            $("#first").val("");
-            $("#last").val("");
-            $("#amount").val("");
-        });
+        $("#display").click(function() {
 
-        // Find and remove selected table rows
-         $("#delete-user").click(function(){
-            $("table tbody").find('input[type="checkbox"]').each(function(){
-                if($(this).is(":checked")){
-                    var curRow = $(this).closest('tr').children("td:first").html();
-                    $(this).parents("tr").next().children("td:first").html(curRow);
-                    $(this).closest('tr').remove();
-
+            $.ajax({
+                url: "input.php", 
+                type: "post",
+                data: {'username': $('#username').val()},                   
+                success: function(data){
+                    if(data == false) {
+                        alert ("Username does not exist.");
+                    } else { 
+                    $("table tbody").append(data);
                 }
+            },
             });
-            rowNum--;
+
         });
-    });
+
+        $("#add, #remove, #delete").click(function() {
+
+            var id=this.id;
+
+            $.ajax({
+                url: "admin.php", 
+                type: "post",
+                data: {'username': $('#username').val(),
+                'amount': $('#amount').val(), 'button': id},      
+                success: function(data){
+                    if(data == false) {
+                    alert("Error: not able to respond to the request.");
+                    } else { 
+                        $("table tbody").append(data);
+                }
+            },
+        });
+
+        });
+
+});
---
+++ AdminPage/admin.php
@@ -0,0 +1,67 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$passward = "";
+$dbname = "myDB";
+$tablename = "WebsiteUsers";
+
+//Create connection
+$connection = new mysqli($servername, $username, $passward, $dbname);
+
+if($_SERVER['REQUEST_METHOD'] == "POST") {
+    $username   = mysqli_real_escape_string ($connection,$_POST['username']);
+    $amount   = mysqli_real_escape_string ($connection,$_POST['amount']);
+    $button   = mysqli_real_escape_string ($connection,$_POST['button']);
+       
+    $query = "SELECT userID,points,lname,fname,username FROM WebsiteUsers where username='$username'";
+    $result = mysqli_query($connection,$query);
+    $numResults = mysqli_num_rows($result);
+
+    if($numResults==1){
+            
+        $row = mysqli_fetch_assoc($result);
+        $user_id =  $row['userID'];
+        $user_points =  $row['points'];
+        $lname =  $row['lname'];
+        $fname =  $row['fname'];
+
+            if(($button == "add") and is_numeric($amount)) {
+            $user_points += $amount;
+            echo $user_points;
+            $sql = "UPDATE WebsiteUsers SET points='$user_points' WHERE userID='$user_id'";
+            }
+            
+            if(($button == "remove") and is_numeric($amount)) {
+                if ($amount<0) $amount= -$amount;
+                $user_points -= $amount;
+                $sql = "UPDATE WebsiteUsers SET points='$user_points' WHERE userID='$user_id'";
+                }
+                
+            if($button == "delete") {
+                $sql = "DELETE FROM WebsiteUsers WHERE userID='$user_id'";
+            }
+        
+
+        if ($connection->query($sql) === TRUE) {
+
+            echo "<tr>";
+            echo "<td>" . $username . "</td>";
+            echo "<td>" . $lname . "</td>";
+            echo "<td>" . $fname . "</td>";
+            echo "<td>" . $user_points . "</td>";
+            echo "</tr>";
+
+        } else {
+            echo false;
+            $connection->error; 
+        }
+
+    } else {
+        echo false;
+        $connection->error; 
+        }
+}   
+
+    $connection->close(); 
+
+?>
